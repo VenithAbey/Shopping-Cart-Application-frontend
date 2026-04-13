e3117395ba8d34b882607f3ebbb6301b32d4ea10
V3N1TH <152465117+VenithAbey@users.noreply.github.com>
2026-04-13 10:02:44 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -16,7 +16,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8080/api/:path*',
+        destination: process.env.NEXT_PUBLIC_API_URL ? `${process.env.NEXT_PUBLIC_API_URL}/:path*` : 'http://localhost:8080/api/:path*',
       },
     ]
   },
